0cf3faa433c9e4d14904c0c14723d8c4b1f31ab4
smirnovrm <r.smirnov86@gmail.com>
2021-01-17 16:06:19 +0600

    добавляет стили для мобильных и планшетов

--- source/js/app.js
+++ source/js/app.js
@@ -5,11 +5,11 @@ let close = document.getElementById("close");
 
 login.addEventListener("click", function(e){
   e.preventDefault();
-  modal.classList.remove("visually-hidden");
+  modal.classList.remove("modal--hide");
   body.classList.add("page__overlay");
 });
 
 close.addEventListener("click", function(){
-  modal.classList.add("visually-hidden");
+  modal.classList.add("modal--hide");
   body.classList.remove("page__overlay");
 });
